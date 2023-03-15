62d31d348c710e421fbe1aa2d162d448e27430fe
fartun <fartunwario34@gmail.com>
2023-03-15 19:28:04 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+console.log(multiply); // Output: 62
+const random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 9 ;
+const mod = num1 % num3 ;
+const num4 = mod ;
+const numbers = [5, 10, 20, 15, 8];
+const max = Math.max(...numbers);
